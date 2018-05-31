2b7cb9b8ad6bb1d8367f370220d16122cf0bfd5d
april <martinezapril56@yahoo.com>
2018-05-31 15:20:15 -0400

    start 2nd page

--- lambda-notes/src/components/NoteView/NoteView.js
+++ lambda-notes/src/components/NoteView/NoteView.js
@@ -2,13 +2,63 @@ import React, { Component } from 'react';
 import './index.css';
 
 class NoteView extends Component {
+    constructor() {
+        super()
+        this.state = {
+            notesArray: [
+                {
+                    _id: 'adshfjslglff;hf;',
+                    title: 'first Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455211,
+                },
+                {
+                    _id: 'jslglff;hf;',
+                    title: 'second Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455211,
+                },
+                {
+                    _id: 'lff;hf;',
+                    title: 'third Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455210,
+                },
+                {
+                    _id: 'aglff;hf;',
+                    title: 'forth Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455208,
+                },
+                {
+                    _id: 'aglff;hf;',
+                    title: 'fifth Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455207,
+                },
+                {
+                    _id: 'aglff;hf;',
+                    title: 'six Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455206,
+                },
+            ]
+        }
+    }
+
+
     render() {
         return (
-            <div>
-                NOTE VIEW PLACEHOLDER
+            <div className='notesList_container'>
+                <div><h3 className='content_header'>Your Notes:</h3></div>
+                <div className='notesList'>
+                   
+                </div>
             </div>
-        );
+                        
+                                   
+        )
     }
-}
+   }
 
 export default NoteView;
--- lambda-notes/src/components/NotesList/NotesList.js
+++ lambda-notes/src/components/NotesList/NotesList.js
@@ -2,53 +2,78 @@ import React, { Component } from 'react';
 import './index.css';
 
 class NotesList extends Component {
+    constructor() {
+        super()
+        this.state = {
+            notesArray: [
+                {
+                    _id: 'adshfjslglff;hf;',
+                    title: 'first Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455211,
+                },
+                {
+                    _id: 'jslglff;hf;',
+                    title: 'second Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455211,
+                },
+                {
+                    _id: 'lff;hf;',
+                    title: 'third Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455210,
+                },
+                {
+                    _id: 'aglff;hf;',
+                    title: 'forth Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455208,
+                },
+                {
+                    _id: 'aglff;hf;',
+                    title: 'fifth Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455207,
+                },
+                {
+                    _id: 'aglff;hf;',
+                    title: 'six Note',
+                    body: 'Lorem  ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh',
+                    createdAt: 1527536455206,
+                },
+            ]
+        }
+    }
+    generateNotes = (what, where) => {
+        return (
+            <div className= 'note' key = { where + what.title } >
+                <div>
+                <h4>{what.title}</h4>
+                    <hr></hr>
+                <p>{what.body}</p>    
+                </div>
+                </div>
+                  )
+              }
+            
+       
     render() {
         return (
             <div className='notesList_container'>
                 <div><h3 className='content_header'>Your Notes:</h3></div>
                 <div className='notesList'>
-                    <div className='note'>
-                        <div>
-                            <h4>Note Title</h4>
-                            <hr></hr>
-                            <p>Lorem ipsum</p>
-                        </div>
-                    </div>
-                    <div className='note'>
-                        <div>
-                            <h4>Note Title</h4>
-                            <hr></hr>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
-                        </div>
-                    </div>
-                    <div className='note'>
-                        <div>
-                            <h4>Note Title</h4>
-                            <hr></hr>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
-                        </div>
-                    </div>
-                    <div className='note'>
-                        <div>
-                            <h4>Note Title</h4>
-                            <hr></hr>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
-                        </div>
-                    </div>
-                    <div className='note'>
-                        <div>
-                            <h4>Note Title</h4>
-                            <hr></hr>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
-                        </div>
-                    </div>
-                    <div className='note'>
-                        <div>
-                            <h4>Note Title</h4>
-                            <hr></hr>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
-                        </div>
-                    </div>
+                    {this.state.notesArray.map((note, index) => {
+                        return (
+                            <div className='note' key={index + note.title}>
+                                <div>
+                                    <h4>{note.title}</h4>
+                                    <hr></hr>
+                                    <p>{note.body}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         );
